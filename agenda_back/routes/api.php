eac61269e4bb4e6554921952144738682cd9c927
Everaldo <torresjr1984@hotmail.com>
2022-11-07 12:30:49 -0300

    Configurado o jwt-auth

--- agenda_back/routes/api.php
+++ agenda_back/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,4 +11,4 @@ Route::get('home', [UserController::class, 'index'])->middleware('auth');
 
 Route::post('register', [RegisterController::class, 'create']);
 
-Route::post('login', [LoginController::class])->name('login');
+Route::post('login', [AuthController::class, 'login']);
