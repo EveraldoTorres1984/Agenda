fcdfe8b2628b9ac0f3cf2dea653753b779f221fd
Everaldo <torresjr1984@hotmail.com>
2022-11-07 11:42:42 -0300

    Configurado o jwt-auth

--- agenda_back/app/Models/User.php
+++ agenda_back/app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -46,4 +46,20 @@ class User extends Authenticatable
     public function schedules(){
         return $this->HasMany(Schedule::class);
     }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
+    
+
