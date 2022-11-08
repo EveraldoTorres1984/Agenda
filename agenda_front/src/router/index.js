acd7eea7eb2e2e2bdc0ae8317653db7f150fb035
Everaldo <torresjr1984@hotmail.com>
2022-11-08 11:33:18 -0300

    Tela de Login finalizada

--- agenda_front/src/router/index.js
+++ agenda_front/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
+import Guard from '../services/middleware'
 
 const routes = [
   {
@@ -13,7 +14,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
+    beforeEnter: Guard.auth
   }
 ]
 
--- agenda_front/src/views/Login.vue
+++ agenda_front/src/views/Login.vue
@@ -6,17 +6,19 @@
         <p class="description description-primary">
           Por favor, faça o Login para ter acesso.
         </p>
-        <form class="form">
+        <form @submit.stop.prevent="VerifyLogin" class="form">
           <label class="label-input" for="">
             <fa icon="envelope" class="icon-modify" />
-            <input type="email" placeholder="Email" />
+            <input v-model="email" type="email" placeholder="Email" />
           </label>
           <label class="label-input" for="">
             <fa icon="lock" class="icon-modify" />
-            <input type="password" placeholder="Senha" />
+            <input type="password" v-model="password" placeholder="Senha" />
           </label>
+          <button type="submit" id="signin" class="btn btn-primary">
+            ENTRAR
+          </button>
         </form>
-        <button id="signin" class="btn btn-primary">ENTRAR</button>
       </div>
       <div class="second-column">
         <h2 class="title title-second">Criar conta</h2>
@@ -33,193 +35,223 @@
 
           <label class="label-input" for="">
             <fa icon="lock" class="icon-modify" />
-            <input type="password" v-model="form.password" placeholder="Senha" />
+            <input
+              type="password"
+              v-model="form.password"
+              placeholder="Senha"
+            />
           </label>
         </form>
-        <button type="button" class="btn btn-second" @click="submitForm">Cadastrar</button>
+        <button type="button" class="btn btn-second" @click="submitForm">
+          Cadastrar
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
+import axios from "axios";
 
-  export default {
-    data() {
-      return {
-        form: {
-          name: "",
-          email: "",
-          password: ""
-        }
-      }
+import Cookies from "js-cookie";
+
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: "",
+      },
+      email: "",
+      password: "",
+    };
+  },
+
+  methods: {
+    submitForm() {
+      axios
+        .post("http://localhost/api/register", this.form)
+        .then(function (response) {
+          window.location = "/home";
+        });
     },
 
-    methods: {
-      submitForm() {
-        axios.post('http://localhost/api/register', this.form)
-        .then(function (response) {
-          window.location = "/localhost:8080/home";
-        })
-      }
-    }
+    VerifyLogin() {
+      let self = this;
 
-  }
+      return new Promise(function (resolve, reject) {
+        axios
+          .post("http://localhost/api/login", {
+            email: self.email,
+            password: self.password,
+          })
+          .then(function (response) {
+            Cookies.set("_myapp_token", response.data.access_token);
+          })
+          .finally(function () {
+            window.location = "/home";
+          });
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-image: url("/public/img/back-agenda.jpg");
-    background-size: cover;
-  }
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url("/public/img/back-agenda.jpg");
+  background-size: cover;
+}
 
-  .content {
-    background-color: #fff;
-    border-radius: 15px;
-    height: 50%;
-    width: 960px;
-    justify-content: space-between;
-    align-items: center;
-    position: relative;
-  }
+.content {
+  background-color: #fff;
+  border-radius: 15px;
+  height: 50%;
+  width: 960px;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+}
 
-  .content::before {
-    content: "";
-    position: absolute;
-    background-color: #1e90ff;
-    width: 50%;
-    height: 100%;
-    border-top-left-radius: 15px;
-    border-bottom-left-radius: 15px;
+.content::before {
+  content: "";
+  position: absolute;
+  background-color: #1e90ff;
+  width: 50%;
+  height: 100%;
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
 
-    left: 0;
-  }
+  left: 0;
+}
 
-  .title {
-    font-size: 28px;
-    font-weight: bold;
-  }
+.title {
+  font-size: 28px;
+  font-weight: bold;
+}
 
-  .title-primary {
-    color: #fff;
-  }
+.title-primary {
+  color: #fff;
+}
 
-  .title-second {
-    color: #1e90ff;
-  }
+.title-second {
+  color: #1e90ff;
+}
 
-  .form {
-    display: flex;
-    flex-direction: column;
-    width: 75%;
-    margin: 15px;
-  }
+.form {
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+  margin: 15px;
+}
 
-  .form input {
-    height: 45px;
-    width: 100%;
-    border: none;
-    background-color: #ecf0f1;
-  }
+.form input {
+  height: 45px;
+  width: 100%;
+  border: none;
+  background-color: #ecf0f1;
+}
 
-  textarea:focus,
-  input:focus,
-  select:focus {
-    box-shadow: 0 0 0 0;
-    border: 0 none;
-    outline: 0;
-  }
+textarea:focus,
+input:focus,
+select:focus {
+  box-shadow: 0 0 0 0;
+  border: 0 none;
+  outline: 0;
+}
 
-  input:focus::placeholder {
-    color: transparent;
-  }
+input:focus::placeholder {
+  color: transparent;
+}
 
-  .label-input {
-    background-color: #ecf0f1;
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-  }
+.label-input {
+  background-color: #ecf0f1;
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+}
 
-  .icon-modify {
-    color: #7f8c8d;
-    padding: 0 5px;
-  }
+.icon-modify {
+  color: #7f8c8d;
+  padding: 0 5px;
+}
 
-  .description {
-    font-size: 14px;
-    font-weight: 300;
-    color: #fff;
-    line-height: 30px;
-  }
+.description {
+  font-size: 14px;
+  font-weight: 300;
+  color: #fff;
+  line-height: 30px;
+}
 
-  .description-primary {
-    color: #fff;
-  }
+.description-primary {
+  color: #fff;
+}
 
-  .btn {
-    border-radius: 15px;
-    text-transform: uppercase;
-    font-size: 10px;
-    padding: 10px 50px;
-    cursor: pointer;
-    font-weight: bold;
-    width: 150px;
-    align-self: center;
-    border: none;
-    margin-top: 1rem;
-  }
+.btn {
+  border-radius: 15px;
+  text-transform: uppercase;
+  font-size: 10px;
+  padding: 10px 50px;
+  cursor: pointer;
+  font-weight: bold;
+  width: 150px;
+  align-self: center;
+  border: none;
+  margin-top: 1rem;
+}
 
-  .btn-primary {
-    background-color: transparent;
-    border: 1px solid #fff;
-    transition: background-color 0.5s;
-  }
+.btn-primary {
+  background-color: transparent;
+  border: 1px solid #fff;
+  transition: background-color 0.5s;
+}
 
-  .btn-primary:hover {
-    background-color: #fff;
-    color: #1e90ff;
-    border: 1px solid #1e90ff;
-  }
+.btn-primary:hover {
+  background-color: #fff;
+  color: #1e90ff;
+  border: 1px solid #1e90ff;
+}
 
-  .btn-second {
-    background-color: #1e90ff;
-    color: #fff;
-    transition: background-color 0.5s;
-  }
+.btn-second {
+  background-color: #1e90ff;
+  color: #fff;
+  transition: background-color 0.5s;
+}
 
-  .btn-second:hover {
-    background-color: #00bfff;
-    color: #fff;
-  }
+.btn-second:hover {
+  background-color: #00bfff;
+  color: #fff;
+}
 
-  .first-content {
-    display: flex;
-  }
+.first-content {
+  display: flex;
+}
 
-  .first-content .second-column {
-    z-index: 11;
-  }
+.first-content .second-column {
+  z-index: 11;
+}
 
-  .first-column {
-    text-align: center;
-    width: 60%;
-    z-index: 10;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-  }
+.first-column {
+  text-align: center;
+  width: 60%;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .second-column {
-    width: 60%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.second-column {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
