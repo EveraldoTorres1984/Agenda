786b27f456ceec8ba8cc5fa4bd984b30f4caeb54
Everaldo <torresjr1984@hotmail.com>
2022-11-11 12:53:01 -0300

    Ajustes na formatação das datas

--- agenda_front/src/views/Home.vue
+++ agenda_front/src/views/Home.vue
@@ -11,18 +11,20 @@
               <th>Data</th>
               <th>Horário</th>
               <th>Descrição</th>
-              <th style="width:200px">Ações</th>
+              <th style="width: 200px">Ações</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="task in tasks" :key="task.id">
-              <td>{{task.title}}</td>
-              <td>{{task.date}}</td>
-              <td>{{task.time}}</td>
-              <td>{{task.description}}</td>
+              <td>{{ task.title }}</td>
+              <td>{{ format_date(task.date) }}</td>
+              <td>{{ task.time }}</td>
+              <td>{{ task.description }}</td>
               <td>
                 <div class="btn-action">
-                  <button class="btn btn-warning" style="margin-right: 10px;">Editar</button>
+                  <button class="btn btn-warning" style="margin-right: 10px">
+                    Editar
+                  </button>
                   <button class="btn btn-danger">Excluir</button>
                 </div>
               </td>
@@ -38,56 +40,54 @@
 import ScheduleForm from "../components/ScheduleForm.vue";
 import axios from "axios";
 import Cookies from "js-cookie";
+import moment from "moment";
 
 export default {
-
-  data(){
-    return{
-      tasks:[]
-    }
+  data() {
+    return {
+      tasks: [],
+    };
   },
   name: "Home",
   components: {
-    ScheduleForm, 
-  },   
+    ScheduleForm,
+  },
 
-  
   created() {
-   this.getSchedules();
-
+    this.getSchedules();
   },
 
   methods: {
     getSchedules() {
-      
-      let token =  Cookies.get("_myapp_token");
-             const config = {
-          headers: {Authorization : `Bearer ${token}`}
-        }
-    axios.get("http://localhost/api/home", config)
-      .then((response) => {
-     this.tasks = response.data[0].schedules
+      let token = Cookies.get("_myapp_token");
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+      axios.get("http://localhost/api/home", config).then((response) => {
+        this.tasks = response.data[0].schedules;
       });
-      }
     },
-  
-  
-};
+    format_date(date) {
+      if(date){
+        return moment(String(date)).format('DD/MM/YYYY')
+      }
+    },    
 
+  },
+
+    };
 </script>
 
 <style scoped>
-.card-body{
+.card-body {
   display: flex;
   justify-content: center;
   text-align: center;
-  padding: 10px;  
+  padding: 10px;
 }
 
-button{
+button {
   height: 32px;
-  font-size: 14px; 
-  
+  font-size: 14px;
 }
-
 </style>
